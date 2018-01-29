dfcf7792faf5965c82dcaa27eb66c1c1800ae923
Paul Hamill <paul.hamill@gatech.edu>
2018-01-29 14:36:32 -0500

    remote db & gitignore

--- sqldb.py
+++ sqldb.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-engine = create_engine('sqlite:///tutorial.db', echo=True)
+engine = create_engine('mysql://jd:jd2018@67.205.168.129/junior_design', echo=True)
 Base = declarative_base()
 
 ########################################################################
@@ -13,14 +13,19 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String)
-    password = Column(String)
+    name = Column(String(4096)) # This & email lengths are arbitrary max lengths. TEXT type has max length of 2^16 (65536)
+    email = Column(String(4096))
+    password = Column(String(64)) # sha256 = 256bits (64 bytes)
+    salt = Column(String(8)) # we'll generate salts that are 8 bytes long
+    oauth = Column(Integer)
+    token = Column(String(32)) # also arbitrary but we should limit it later!
 
     #----------------------------------------------------------------------
-    def __init__(self, username, password):
+    def __init__(self, name, email, password):
         """"""
-        self.username = username
+        self.username = name
         self.password = password
+        self.email = email
 
 # create tables
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
