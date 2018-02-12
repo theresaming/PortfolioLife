73a744d3bedc8aedcbc497c8c0f8ad160c72fcf7
theresaming <theresaming@gmail.com>
2018-02-11 19:57:56 -0500

    connect registration to api

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import flash, redirect, render_template, request, session, abort, jsonify, json
+import httplib, urllib
+import requests
 import os
 from sqlalchemy.orm import sessionmaker
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
-# from sqldb  import *
+from sqldb  import *
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
@@ -32,75 +34,6 @@ app.config['OAUTH_CREDENTIALS'] = {
         'secret': 'QpD_M7SzNVpmMVNPXTrJmy1m'
     }
 }
-class OAuthSignIn(object):
-    providers = None
-
-    def __init__(self, provider_name):
-        self.provider_name = provider_name
-        credentials = current_app.config['OAUTH_CREDENTIALS'][provider_name]
-        self.consumer_id = credentials['id']
-        self.consumer_secret = credentials['secret']
-
-    def authorize(self):
-        pass
-
-    def callback(self):
-        pass
-
-    def get_callback_url(self):
-        return url_for('oauth_callback', provider=self.provider_name,
-                       _external=True)
-
-    @classmethod
-    def get_provider(self, provider_name):
-        if self.providers is None:
-            self.providers = {}
-            for provider_class in self.__subclasses__():
-                provider = provider_class()
-                self.providers[provider.provider_name] = provider
-        return self.providers[provider_name]
-
-class FacebookSignIn(OAuthSignIn):
-    def __init__(self):
-        super(FacebookSignIn, self).__init__('facebook')
-        self.service = OAuth2Service(
-            name='facebook',
-            client_id=self.consumer_id,
-            client_secret=self.consumer_secret,
-            authorize_url='https://graph.facebook.com/oauth/authorize',
-            access_token_url='https://graph.facebook.com/oauth/access_token',
-            base_url='https://graph.facebook.com/'
-        )
-    def authorize(self):
-        return redirect(self.service.get_authorize_url(
-            scope='email',
-            response_type='code',
-            redirect_uri=self.get_callback_url())
-    )
-
-    def callback(self):
-        def decode_json(payload):
-            return json.loads(payload.decode('utf-8'))
-
-        if 'code' not in request.args:
-            return None, None, None
-        oauth_session = self.service.get_auth_session(
-            data={'code': request.args['code'],
-                  'grant_type': 'authorization_code',
-                  'redirect_uri': self.get_callback_url()},
-            decoder=decode_json
-        )
-        me = oauth_session.get('me').json()
-        return (
-            'facebook$' + me['id'],
-            me.get('email').split('@')[0],  # Facebook does not provide
-                                            # username, so the email's user
-                                            # is used instead
-            me.get('email')
-        )
-
-class GoogleSignIn(OAuthSignIn):
-    pass
 
 @app.route('/')
 def home():
@@ -124,63 +57,64 @@ def do_admin_login():
         flash('wrong password!')
     return home()
 
-@app.route('/registration', methods=['GET', 'POST'])
+@app.route('/registration', methods=['GET','POST'])
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST':
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        user = User(form.username.data, form.password.data)
-        session.add(user)
-        session.commit()
-        flash('Thanks for registering')
-        return render_template('login.html')
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        data = {
+            'email': email,
+            'name': username,
+            'password': password,
+            }
+        jsonStr = json.dumps(data)
+        r = requests.post("http://67.205.168.129:8080/user/register", jsonStr)
+        jsonDict = json.loads(r.text)
+        print "jsonDict: ", jsonDict
+        print "jsonDict['message']", jsonDict['message']
+        print "jsonDict['success']", jsonDict['success']
+        if jsonDict['success'] == True:
+            flash('Thanks for registering')
+            return render_template('login.html')
+        else:
+            flash(jsonDict['message'])
     return render_template('registration.html', form=form)
 
-@app.route('/authorize/facebook')
-def oauth_authorize(provider):
-    if not current_user.is_anonymous():
-        return redirect(url_for('login'))
-    oauth = OAuthSignIn.get_provider(provider)
-    return oauth.authorize()
+@app.route('/api/register', methods=['POST'])
+def reg():
+    content = request.get_json()
+    print (content)
 
-@app.route('/callback/facebook')
-def oauth_callback(provider):
-    if not current_user.is_anonymous():
-        return redirect(url_for('index'))
-    oauth = OAuthSignIn.get_provider(provider)
-    social_id, username, email = oauth.callback()
-    if social_id is None:
-        flash('Authentication failed.')
-        return redirect(url_for('logout'))
-    user = User.query.filter_by(social_id=social_id).first()
-    if not user:
-        user = User(social_id=social_id, nickname=username, email=email)
-        db.session.add(user)
-        db.session.commit()
-    login_user(user, True)
-    return redirect(url_for('login'))
+# @app.route('/authorize/facebook')
+# def oauth_authorize(provider):
+#     if not current_user.is_anonymous():
+#         return redirect(url_for('login'))
+#     oauth = OAuthSignIn.get_provider(provider)
+#     return oauth.authorize()
+#
+# @app.route('/callback/facebook')
+# def oauth_callback(provider):
+#     if not current_user.is_anonymous():
+#         return redirect(url_for('index'))
+#     oauth = OAuthSignIn.get_provider(provider)
+#     social_id, username, email = oauth.callback()
+#     if social_id is None:
+#         flash('Authentication failed.')
+#         return redirect(url_for('logout'))
+#     user = User.query.filter_by(social_id=social_id).first()
+#     if not user:
+#         user = User(social_id=social_id, nickname=username, email=email)
+#         db.session.add(user)
+#         db.session.commit()
+#     login_user(user, True)
+#     return redirect(url_for('login'))
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
     return home()
 
-# @app.route('/test')
-# def test():
-#
-#     POST_USERNAME = "python"
-#     POST_PASSWORD = "python"
-#
-#     Session = sessionmaker(bind=engine)
-#     s = Session()
-#     query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-#     result = query.first()
-#     if result:
-#         return "Object found"
-#     else:
-#         return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
-
 if __name__ == "__main__":
-
     app.run(debug=True,host='0.0.0.0', port=4000)
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export FLASK_DEBUG=1
+FLASK_APP=flask/app.py flask run
