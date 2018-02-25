477609b78a22e3c4d62665efed6c046688a1d12f
Paul Hamill <paul.hamill@gatech.edu>
2018-02-25 15:57:29 -0500

    work on picture mass deletion

--- server/picture_test.go
+++ server/picture_test.go
@@ -7,3 +7,18 @@ import (
 func TestDeletePictureFromS3(t *testing.T) {
 	deleteFromS3("users/3/buzz.png")
 }
+
+func TestDeletePicturesFromS3(t *testing.T) {
+	pics := []Picture{
+		Picture{
+			ImagePath: "users/3/buzz.png",
+		},
+		Picture{
+			ImagePath: "users/3/buzz.png",
+		},
+	}
+	err := deleteMultipleFromS3(pics)
+	if err != nil {
+		panic(err)
+	}
+}
