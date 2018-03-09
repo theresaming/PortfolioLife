28925e61f0d0a0eda0276753e1b84f35dd9e96ca
messue <melissa.a.e96@gmail.com>
2018-03-09 02:12:22 -0500

    made registration placeholders match home

--- flask/registration.py
+++ flask/registration.py
@@ -2,10 +2,10 @@ from wtforms import Form, StringField, PasswordField, validators
 
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)], description = "name", render_kw={"placeholder": "Name"})
-    email = StringField('Email Address', [validators.Length(min=6, max=35)], description = "email", render_kw={"placeholder": "Email Address"})
+    username = StringField('Username', [validators.Length(min=4, max=25)], description = "name", render_kw={"placeholder": "name"})
+    email = StringField('Email Address', [validators.Length(min=6, max=35)], description = "email", render_kw={"placeholder": "email"})
     password = PasswordField('Create Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
-    ], description = "password", render_kw={"placeholder": "Password"})
-    confirm = PasswordField('Confirm Password', description = "Confirm password", render_kw={"placeholder": "Retype Password"})
+    ], description = "password", render_kw={"placeholder": "password"})
+    confirm = PasswordField('Confirm Password', description = "Confirm password", render_kw={"placeholder": "retype password"})
