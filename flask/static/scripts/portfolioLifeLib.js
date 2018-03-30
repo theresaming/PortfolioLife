4d4fb83b924a64ee90eff73cf77447cff8506081
Joshua Koh <Joshua@kohfamily.com>
2018-03-30 11:44:32 -0400

    Add back button validation, Submit validation

--- flask/static/scripts/portfolioLifeLib.js
+++ flask/static/scripts/portfolioLifeLib.js
@@ -7,3 +7,23 @@ function checkImageSelect(clicked_id) {
         image.style.border = "3px solid #FFFFFF";
     }
 }
+
+function albumValidation() {
+    var title = document.getElementById("title-text");
+    if ("" == title.value) {
+        title.style.border = "1px solid red";
+        alert("Please enter an album title.");
+        return false;
+    }
+
+    var checkBoxes = document.getElementsByClassName( 'imgCheckbox' );
+    console.log(checkBoxes.length);
+    for (var i = 0; i < checkBoxes.length; i++) {
+        if ( checkBoxes[i].checked ) {
+            return true;
+        }
+    }
+    alert( 'Please select at least one picture!' );
+    return false;
+
+}
