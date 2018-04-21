e051b262fb0ff8d35c26af75d43b9bee176c7570
Joshua Koh <Joshua@kohfamily.com>
2018-04-21 15:19:40 -0400

    Incorporate Google Web S2T

--- flask/static/scripts/voice-control.js
+++ flask/static/scripts/voice-control.js
@@ -1,156 +1,80 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;
 
-var audioContext = new AudioContext();
-var audioInput = null,
-    realAudioInput = null,
-    inputPoint = null,
-    audioRecorder = null;
-var rafID = null;
-var analyserContext = null;
-var canvasWidth, canvasHeight;
-var recIndex = 0;
-var toggledOnce = false;
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
+} else {
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
 
+  recognition.onstart = function() {
+    recognizing = true;
+  };
 
-function exportAudio() {
-    audioRecorder.exportWAV( doneEncoding );
-}
-
-function doneEncoding( blob ) {
-    Recorder.setupDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
-    recIndex++;
-}
-
-function toggleRecording( e ) {
-    var analyzer = document.getElementById("analyzer");
-    var button = document.getElementById("voice-button");
-
-    if (e.classList.contains("recording")) {
-        // stop recording
-        audioRecorder.stop();
-        e.classList.remove("recording");
-        audioRecorder.getBuffers( exportAudio );
-        analyzer.style.visibility = "hidden";
-        button.innerText = "How can we help you?";
-    } else {
-        // start recording
-        if (!audioRecorder)
-            return;
-        e.classList.add("recording");
-        audioRecorder.clear();
-        audioRecorder.record();
-        analyzer.style.visibility = "visible";
-        button.innerText = "Listening...";
+  recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+      ignore_onend = true;
     }
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function activateSubmit() {
-    console.log("activate");
-    if (toggledOnce) {
-        console.log("toggled");
-        await sleep(200);
-        document.getElementById("audioForm").submit();
-    } else {
-        console.log("not tog");
-        toggledOnce = true;
+    if (event.error == 'audio-capture') {
+      ignore_onend = true;
     }
-}
-
-function updateAnalysers(time) {
-    if (!analyserContext) {
-        var canvas = document.getElementById("analyzer");
-
-        canvasWidth = parseFloat($('.voice-vis').css('width'));
-        canvasHeight = parseFloat($('.voice-vis').css('height'));
-        analyserContext = canvas.getContext('2d');
+    if (event.error == 'not-allowed') {
+      ignore_onend = true;
     }
+  };
 
-    // analyzer draw code here
-    {
-        var SPACING = 3;
-        var BAR_WIDTH = 1;
-        var numBars = Math.round(canvasWidth / SPACING);
-        var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
-
-        analyserNode.getByteFrequencyData(freqByteData);
-
-        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        analyserContext.fillStyle = '#F6D565';
-        analyserContext.lineCap = 'round';
-        var multiplier = analyserNode.frequencyBinCount / numBars;
-
-        // Draw rectangle for each frequency bin.
-        for (var i = 0; i < numBars; ++i) {
-            var magnitude = 0;
-            var offset = Math.floor( i * multiplier );
-            // gotta sum/average the block, or we miss narrow-bandwidth spikes
-            for (var j = 0; j< multiplier; j++)
-                magnitude += freqByteData[offset + j];
-            magnitude = magnitude / multiplier;
-            var magnitude2 = freqByteData[i * multiplier];
-            analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
-            analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
-        }
+  recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+      return;
     }
-
-    rafID = window.requestAnimationFrame( updateAnalysers );
-}
-
-function gotStream(stream) {
-    inputPoint = audioContext.createGain();
-
-    // Create an AudioNode from the stream.
-    realAudioInput = audioContext.createMediaStreamSource(stream);
-    audioInput = realAudioInput;
-    audioInput.connect(inputPoint);
-
-    analyserNode = audioContext.createAnalyser();
-    analyserNode.fftSize = 2048;
-    inputPoint.connect( analyserNode );
-
-    audioRecorder = new Recorder( inputPoint );
-
-    zeroGain = audioContext.createGain();
-    zeroGain.gain.value = 0.0;
-    inputPoint.connect( zeroGain );
-    zeroGain.connect( audioContext.destination );
-    updateAnalysers();
-}
-
-function initAudio() {
-    if (!navigator.getUserMedia)
-        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    if (!navigator.cancelAnimationFrame)
-        navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-    if (!navigator.requestAnimationFrame)
-        navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
-
-    navigator.getUserMedia(
-        {
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
-                },
-                "optional": []
-            },
-        }, gotStream, function(e) {
-            alert('Error getting audio');
-            console.log(e);
-        });
-}
-
-
-window.addEventListener('load', initAudio );
-
-window.addEventListener('keypress', function (e) {
-    if (e.keyCode == 32) {
-        toggleRecording(document.getElementById("voice-button"));
+    if (!final_transcript) {
+      return;
     }
-}, false);
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+      var range = document.createRange();
+      range.selectNode(document.getElementById('final_span'));
+      window.getSelection().addRange(range);
+    }
+  };
+
+  recognition.onresult = function(event) {
+    var interim_transcript = '';
+    if (typeof(event.results) == 'undefined') {
+      recognition.onend = null;
+      recognition.stop();
+      upgrade();
+      return;
+    }
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+        document.getElementById("results-viewer").textContent=interim_transcript;
+      }
+    }
+    document.getElementById("results-viewer").textContent=final_transcript;
+    final_span.innerHTML = final_transcript;
+    interim_span.innerHTML = interim_transcript;
+  };
+}
+
+function startButton(event) {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+  final_transcript = '';
+  recognition.lang = 'en-US';
+  recognition.start();
+  ignore_onend = false;
+  final_span.innerHTML = '';
+  interim_span.innerHTML = '';
+  start_timestamp = event.timeStamp;
+  document.getElementById("results-viewer").textContent="Listening...";
+}
